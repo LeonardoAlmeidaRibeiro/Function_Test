35370e32fac5d63550d5467bc6883c76a11ecfba
leonardo.a.ribeiro.21@gmail.com <leonardo.a.ribeiro.21@gmail.com>
2020-09-25 14:03:47 -0300

    método com a função

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,11 +3,11 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
-import util.UperCaseName;
+import model.services.ProductService;
+
 
 public class Program {
 
@@ -21,10 +21,13 @@ public class Program {
 		list.add(new Product ("Tablet",350.00));
 		list.add(new Product ("HD Case", 80.90));
 		
+		ProductService ps = new ProductService();
 		
+		double sum = ps.filteredSum(list);
 		
-		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
-		names.forEach(System.out:: println);
+		System.out.println("Sum = "  + String.format("%.2f", sum));
+		
+	
 	}
 
 }
